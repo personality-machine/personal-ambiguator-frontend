40aaa9e24bf3521712d03875592a6813a4024a88
KyraZzz <yz709@cam.ac.uk>
2022-02-06 17:22:54 +0000

    add static filters to the captured picture

--- root-app/src/recorder.js
+++ root-app/src/recorder.js
@@ -5,31 +5,31 @@ import './recorder.css'
 const Recorder = () => {
     const webcamRef = React.useRef(null); // persistent reference cause no rerendering
     const [imgSrc, setImgSrc] = React.useState(null);
-    
-    const displayImage = () => {
-        document.getElementsByClassName("image").style.filter = "opacity(50%)";
-        document.getElementsByClassName("image").style.filter = "contrast(50%)";
-    }
+    const [contrast, setContrast] = React.useState(100);
+    const [brightness, setBrightness] = React.useState(100);
+    const [saturate, setSaturate] = React.useState(100);
+
+    const filters = {
+        filter: ('contrast(' + contrast + '%)', 'brightness(' + brightness + '%)', 'saturate(' + saturate + '%)')
+    };
 
     const capture = React.useCallback(() => { 
         const imgSrc = webcamRef.current.getScreenshot();
         setImgSrc(imgSrc);
-        displayImage();
+        setContrast(50);
+        setBrightness(50);
+        setSaturate(200);
     }, [webcamRef, setImgSrc]);
 
     return (
-        <div>
+    <div>
       <Webcam
         audio={false}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
       />
       <button onClick={capture}>Capture photo</button>
-      {imgSrc && (
-        <img
-          src={imgSrc} alt="" className="image"
-        />
-      )}
+      {imgSrc && (<img src={imgSrc} alt="" style={filters}/>)}
     </div>
     );
 };
