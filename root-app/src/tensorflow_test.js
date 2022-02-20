2c4cbfa5c1930ce6178c9313bac8317ae9c93e25
minglechen <minglechen2002@gmail.com>
2022-02-20 15:20:26 +0000

    add preliminary APR model

--- root-app/src/tensorflow_test.js
+++ root-app/src/tensorflow_test.js
@@ -6,7 +6,7 @@ const Predict =
     async (imgUri) => {
         var img = new Image();
         img.src = imgUri;
-        const model = await tf.loadGraphModel('test/model.json');
+        const model = await tf.loadLayersModel('no_randomcrop/model.json');
         var tensor = await tf.browser.fromPixelsAsync(img);
         tensor = tensor.slice([0,0,0], [224,224,3]).cast('float32');
         return model.predict(tensor.reshape([1,224,224,3]));
