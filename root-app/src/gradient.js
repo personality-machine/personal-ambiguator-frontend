2ff3e3bf42884001801958b7e60832d0dba0097e
minglechen <minglechen2002@gmail.com>
2022-02-22 15:03:09 +0000

    add ability for gradient on individual personality

--- root-app/src/gradient.js
+++ root-app/src/gradient.js
@@ -2,18 +2,20 @@ import * as tf from '@tensorflow/tfjs';
  
 
 const Gradient =
-    async (imgUri) => {
+    async (imgUri, pNum) => {
+        console.assert(pNum >=0 && pNum < 6);
         var img = new Image();
         img.src = imgUri;
         const model = await tf.loadLayersModel('no_randomcrop/model.json');
         var tensor = await tf.browser.fromPixelsAsync(img);
         tensor = tensor.slice([0,0,0], [224,224,3]).cast('float32').reshape([1,224,224,3]);
-        // calculate gradient
-        let f = x=> model.predict(x);
-        let g = tf.grad(f);
-        var saliency = g(tensor);
 
-        return saliency;
+        // calculate gradient
+        let f = x=> model.predict(x).slice([0,pNum],[1,1]);
+        let g = tf.grad(f);
+        var result = g(tensor);
+
+        return result.array();
     }
 
 
